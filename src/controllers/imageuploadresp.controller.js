406cfdbeaa17ffb6074f59a4d92668b72ac88aec
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-09-21 11:16:49 +0530

    imageupload Controller and routes

--- src/controllers/imageuploadresp.controller.js
+++ src/controllers/imageuploadresp.controller.js
@@ -4,17 +4,13 @@ import request from "request-promise"; // or consider using axios or node-fetch
 
 export const uploadimage = async function (req, res, next) {
   try {
-    const { name = "Plant", photo } = req.body;
+    const { photo } = req.body;
 
     if (!photo) {
       return next(new AppError("Image URL is required", 400));
     }
 
     // Step 1: Save image data in the database
-    const newimage = await Imageupload.create({
-      name,
-      photo,
-    });
 
     // Step 2: Send the Cloudinary URL to your ML model for inference
     const options = {
@@ -28,18 +24,41 @@ export const uploadimage = async function (req, res, next) {
 
     try {
       const prediction = await request(options);
-
       // Step 3: Return the response including ML predictions
+
+      const newimage = await Imageupload.create({
+        name: prediction[0]["class_name"],
+        photo,
+      });
+      const newimageget = await Imageupload.findById(newimage._id).select(
+        "photo name -_id"
+      );
       res.status(201).json({
         status: "success",
-        data: newimage,
-        prediction, // Include the ML model's predictions
+        data: newimageget,
+        prediction: {
+          classId: prediction[0]["class_id"],
+          name: prediction[0]["class_name"],
+          confidence: `${prediction[0]["confidence"] * 100}%`,
+          box: prediction[0]["box"],
+        }, // Include the ML model's predictions
       });
     } catch (mlError) {
       next(new AppError("Error from ML model: " + mlError.message, 500));
     }
   } catch (error) {
     console.log(error);
+    next(new AppError(error.message, 500, error));
+  }
+};
+export const deleteall = async function (req, res, next) {
+  try {
+    await Imageupload.deleteMany({});
+    res.status(200).json({
+      status: "success",
+      message: "All info and images deleted",
+    });
+  } catch (error) {
     next(new AppError(error.message, 500, error));
   }
 };
--- src/routes/imageupload.route.js
+++ src/routes/imageupload.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { uploadimage } from "../controllers/imageuploadresp.controller.js";
+import { deleteall, uploadimage } from "../controllers/imageuploadresp.controller.js";
 import { uploadphoto } from "../utils/multer.js";
 import { uploadphotomidd } from "../middlewares/upload.middleware.js";
 
 const router = express.Router();
 
-router.route("/").post(uploadphoto,uploadphotomidd,uploadimage)
+router.route("/").post(uploadphoto,uploadphotomidd,uploadimage).delete(deleteall);
 
 export default router;
